cc4c6c1513c7dbd6f5e7c8e9fb82a0ce55454570
LilasBe <lilasberakdar1995@gmail.com>
2025-12-22 18:50:14 +0100

    Update RadioController.java

--- src/main/java/app/controllers/RadioController.java
+++ src/main/java/app/controllers/RadioController.java
@@ -1,8 +1,29 @@
 package app.controllers;
 
+import app.models.TrackInfo;
 import app.services.RadioSverigeService;
+import app.services.SpotifyService;
+import io.javalin.Javalin;
+
+import io.javalin.http.Context;
 
 public class RadioController {
     private RadioSverigeService radioSverigeService;
-
+    private SpotifyService spotifyService;
+    private Javalin app;
+    
+    public RadioController(Javalin app,RadioSverigeService radioSverigeService, SpotifyService spotifyService) {
+        this.app = app;
+        this.radioSverigeService = radioSverigeService;
+        this.spotifyService = spotifyService;
+    }
+    public void getDefaultTrack(Context ctx, String chanelId) {
+        TrackInfo track = radioSverigeService.getCurrentTrack("");
+        String trackName= track.getArtist();
+        if (track != null) {
+            ctx.json(track);
+        }else {
+            ctx.status(404).result("Track not found");
+        }
+    }
 }
